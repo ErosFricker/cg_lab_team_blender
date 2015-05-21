b1af692d0e9e28ed166523778ca5e46646031ff3
erosfricker <erosfricker@gmail.com>
2015-05-21 21:27:02 +0200

    Added ParticleEngine class, conforms to WiggleCube example

--- Flamework/Implementation/ParticleEngine.h
+++ Flamework/Implementation/ParticleEngine.h
@@ -25,7 +25,50 @@
 #include "globals.h"
 #include "EmitterParticle.h"
 
+class ParticleEngine : public Model {
+    
+    
+private:
 
+
+    explicit ParticleEngine(SceneManager *sceneManager);
+
+    
+    
+    //Returns the current color of particles produced by the fountain.
+    vmml::vec3f curColor();
+    //Returns the average velocity of particles produced by the fountain.
+    vmml::vec3f curVelocity();
+    
+    //Alters p to be a particle newly produced by the fountain.
+    void createParticle(EmitterParticle* p);
+    
+    //Advances the particle fountain by STEP_TIME seconds.
+    void step();
+    
+    
+public:
+  /*  ParticleEngine(GLuint textureId1) {
+        textureId = textureId1;
+        timeUntilNextStep = 0;
+        colorTime = 0;
+        angle = 0;
+        for(int i = 0; i < NUM_PARTICLES; i++) {
+            createParticle(particles + i);
+        }
+        for(int i = 0; i < 5 / STEP_TIME; i++) {
+            step();
+        }
+    }*/
+    
+    //Advances the particle fountain by the specified amount of time.
+    void advance(float dt);
+    
+    //Draws the particle fountain.
+    void draw();
+};
+
+typedef std::shared_ptr<ParticleEngine> ParticleEnginePtr;
 
 
 
